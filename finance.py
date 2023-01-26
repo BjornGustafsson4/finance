bb0254ae5f4c9d7042400a7de2cf4e3b933262de
BjornGustafsson4 <obxband102@gmail.com>
2023-01-26 14:47:37 -0500

    Fixed some small issues in graphs

--- finance.py
+++ finance.py
@@ -134,7 +134,7 @@ for ticker in ticker_list:
 
     if len(ticker_list) >= 2:
         tick_info = yf.Ticker(ticker).info
-        change_lst = pd.DataFrame([{'ticker': ticker, 'start_date': stock_df['Datetime'].iloc[0], 'end_date': stock_df['Datetime'].iloc[-1], 'percent_change': change_prcnt, 'close_price':round(stock_df['Close'].iloc[-1], 2), 'revenue_per_share':tick_info['revenuePerShare'], 'bv': tick_info['bookValue'], 'pe_ratio':tick_info['trailingEps']}])
+        change_lst = pd.DataFrame([{'ticker': ticker, 'start_date': stock_df['Datetime'].iloc[0], 'end_date': stock_df['Datetime'].iloc[-1], 'percent_change': change_prcnt, 'close_price':round(stock_df['Close'].iloc[-1], 2), 'revenue_per_share':tick_info['revenuePerShare'], 'de': round(tick_info['debtToEquity'], 2), 'pe_ratio':round(tick_info['trailingPE'], 2), 'bv': round(tick_info['bookValue'], 2), 'roe': (round(tick_info['returnOnEquity'], 2) * 100)}])
         change_df = pd.concat([change_df, change_lst])
 
 
@@ -142,14 +142,12 @@ for ticker in ticker_list:
 #Add new stuff here for stock comparison
 if len(ticker_list) >= 2:
     change_df['color_prcnt']= np.where(change_df["percent_change"]<0, 'red', 'green')
-    print(change_df)
 
     ticker_names= ""
     for tick in ticker_list:
         ticker_names += f" {tick}"
-    print(ticker_names)
-    bars_fig = make_subplots(rows=2, cols=2, 
-        subplot_titles=("Percent change ", "Closing price", "P/E Ratio", "Book Value"))
+    bars_fig = make_subplots(rows=3, cols=2, 
+        subplot_titles=("Percent change ", "Closing price", "P/E Ratio", "Debt to Equity", "Return on Equity", "Book Value"))
 
     bars_fig.add_trace(go.Bar(
         x= change_df['ticker'],
@@ -172,13 +170,25 @@ if len(ticker_list) >= 2:
 
     bars_fig.add_trace(go.Bar(
         x= change_df['ticker'],
+        y= change_df['de'], 
+        text= change_df['de']),
+        row= 2, col= 2)
+
+    bars_fig.add_trace(go.Bar(
+        x= change_df['ticker'],
+        y= change_df['roe'], 
+        text= change_df['roe']),
+        row= 3, col= 1)
+
+    bars_fig.add_trace(go.Bar(
+        x= change_df['ticker'],
         y= change_df['bv'], 
         text= change_df['bv']),
-        row= 2, col= 2)
+        row= 3, col= 2)
 
     bars_fig.update_yaxes(ticksuffix="%", row=1, col=1)
     bars_fig.update_yaxes(tickprefix="$", row=1, col=2)
-    bars_fig.update_yaxes(tickprefix="$", row=2, col=2)
+    bars_fig.update_yaxes(ticksuffix="%", row=3, col=1)
 
     pe_mean = round(change_df['pe_ratio'].mean(), 2)
     bars_fig.add_hline(
@@ -190,4 +200,3 @@ if len(ticker_list) >= 2:
     bars_fig.update_layout(title_text=f"{ticker_names} stock comparison over {time_period}")
     bars_fig.update_layout(showlegend=False)
     bars_fig.show()
-    change_df.to_csv("stock_change.csv")
